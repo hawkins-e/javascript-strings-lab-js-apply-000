69f95ff26dd1109887e9f73a019756c2dca91146
Erica Hawkins <ehawk901@gmail.com>
2019-04-13 00:33:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var greeting = "Hello, everybody!"; 
 var specialGuest = "Hello, Neil deGrasse Tyson!";
 
-var greetSpecialGuest = "Hello" + specialGuest + "!";
+var greetSpecialGuest = `Hello, 
+${specialGuest}! High 
+${3+2}!`
 
 var topic = "space";
 var conversation = `${topic}`;
